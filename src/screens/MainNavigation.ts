85649f4b7b9a449a87e137209d19dc460c362e5a
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-12-06 08:50:57 -0300

    update pacakges

--- src/screens/MainNavigation.ts
+++ src/screens/MainNavigation.ts
@@ -1,10 +1,9 @@
-import MainAppTabNavigator from './MainAppTabNavigator';
 import { createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
 import { createBrowserApp } from '@react-navigation/web';
 
 const createApp = Platform.select({
-  web: routes => createBrowserApp(routes, { history: 'hash' }),
-  default: routes => createAppContainer(routes),
+	web: (routes) => createBrowserApp(routes, { history: 'hash' }),
+	default: (routes) => createAppContainer(routes)
 });
-export default createApp(MainAppTabNavigator);
+export default createApp;
